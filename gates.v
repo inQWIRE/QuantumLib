55bf7cf844084d260e292d9973e11b524b103277
jakezweifler <jake.zweifler@gmail.com>
2020-10-13 16:28:54 -0500

    added more lma attempts and completed part of ex 1

--- gates.v
+++ gates.v
@@ -7,6 +7,8 @@ Require Import Matrix.
 
 
 
+
+
 Notation "√ n" := (sqrt n) (at level 20) : R_scope.
 
 Infix "∘" := dot (at level 40, left associativity) : matrix_scope.
@@ -62,12 +64,28 @@ Definition CNOT1 : Matrix 4 4 :=
     [RtoC 0; RtoC 0; RtoC 0; RtoC 1];
     [RtoC 0; RtoC 0; RtoC 1; RtoC 0]]).
 
+
 Definition CNOT2 : Matrix 4 4 := 
   list2D_to_matrix  
   ([[RtoC 1; RtoC 0; RtoC 0; RtoC 0];
     [RtoC 0; RtoC 0; RtoC 0; RtoC 1];
     [RtoC 0; RtoC 0; RtoC 1; RtoC 0];
     [RtoC 0; RtoC 1; RtoC 0; RtoC 0]]).
+
+Definition X1 : Matrix 4 4 := 
+  list2D_to_matrix  
+  ([[RtoC 0; RtoC 0; RtoC 1; RtoC 0];
+    [RtoC 0; RtoC 0; RtoC 0; RtoC 1];
+    [RtoC 1; RtoC 0; RtoC 0; RtoC 0];
+    [RtoC 0; RtoC 1; RtoC 0; RtoC 0]]).
+
+Definition X2 : Matrix 4 4 := 
+  list2D_to_matrix  
+  ([[RtoC 0; RtoC 1; RtoC 0; RtoC 0];
+    [RtoC 1; RtoC 0; RtoC 0; RtoC 0];
+    [RtoC 0; RtoC 0; RtoC 0; RtoC 1];
+    [RtoC 0; RtoC 0; RtoC 1; RtoC 0]]).
+
 
 Definition X (n : nat) : Matrix 4 4 :=
   match n with 
@@ -83,42 +101,201 @@ Definition Z (n : nat) : Matrix 4 4 :=
   | _ => I 4
   end.
 
-(* Lemmas about Pauli and other common matrices *) 
 
+
+
+
+
+
+(****************************)
+(* ATTEMPTS AT REFINING LMA *)
+(****************************)
+
+
+
+(* we need this for some reason... I assume there is a built in tactic that does this*)
 Lemma Propff : forall (b : bool), 
   (if b then false else false) = false.
 Proof. destruct b; reflexivity; reflexivity.
 Qed.
 
 
-Lemma XtimesXid : Xgate × Xgate = I 2.
-Proof. compute. prep_matrix_equality. do 3 (try destruct x; try destruct y; simpl; trivial). 
-       all : (try lca). rewrite -> Propff. lca.
+
+(* added extra tactic to prevent stuckness at if _ then false else false lines *)
+Ltac destruct_m_eq2 := repeat (destruct_m_1; simpl; try lca; try (rewrite -> Propff)).
+
+Ltac lma2 :=
+  compute;
+  autounfold with U_db;
+  prep_matrix_equality;
+  destruct_m_eq2;
+  try lca.
+
+
+
+(*stuff to deal with divmod problems*)
+
+Lemma divmod_eq : forall x y n z, 
+  fst (Nat.divmod x y n z) = (n + fst (Nat.divmod x y 0 z))%nat.
+Proof.
+  induction x.
+  + intros. simpl. lia.
+  + intros. simpl. 
+    destruct z.
+    rewrite IHx.
+    rewrite IHx with (n:=1%nat).
+    lia.
+    rewrite IHx.
+    reflexivity.
 Qed.
 
+Lemma divmod_S : forall x y n z, 
+  fst (Nat.divmod x y (S n) z) = (S n + fst (Nat.divmod x y 0 z))%nat.
+Proof. intros. apply divmod_eq. Qed.
+
+Ltac destruct_m_1' :=
+  match goal with
+  | [ |- context[match ?x with 
+                 | 0   => _
+                 | S _ => _
+                 end] ] => is_var x; destruct x
+  | [ |- context[match fst (Nat.divmod ?x _ _ _) with 
+                 | 0   => _
+                 | S _ => _
+                 end] ] => is_var x; destruct x
+  end.
+
+Lemma divmod_0q0 : forall x q, fst (Nat.divmod x 0 q 0) = (x + q)%nat. 
+Proof.
+  induction x.
+  - intros. simpl. reflexivity.
+  - intros. simpl. rewrite IHx. lia.
+Qed.
+
+Lemma divmod_0 : forall x, fst (Nat.divmod x 0 0 0) = x. 
+Proof. intros. rewrite divmod_0q0. lia. Qed.
+
+
+
+(*must prove but is definitely true. Will do later... may be difficult*)
+(*also I probably shouldn't have to prove this random thing, but it works*)
+(*does it generalize? Kron makes this very bad...*) 
+Lemma resolve_divmod : forall y : nat,
+    Nat.divmod y 1 2 0 = ((2 + y / 2 + (y mod 2%nat))%nat, y mod 2%nat).
+Proof. Admitted.
+
+
+
+
+Ltac destruct_m_eq' := repeat 
+                         (progress (try destruct_m_1'; try rewrite divmod_0; try rewrite divmod_S ; try rewrite resolve_divmod; simpl)).
+
+
+
+Ltac destruct_m_eq_super := repeat (destruct_m_eq'; destruct_m_eq2).  
+
+Ltac lma_super :=
+  compute;
+  autounfold with U_db;
+  prep_matrix_equality;
+  destruct_m_eq2;
+  destruct_m_eq_super;    (* <---- For some reason adding this broke things... *)
+  try lca. 
+                                    
+
+
+
+
+
+
+
+Ltac solve_end :=
+  match goal with
+  | H : lt _ O |- _ => apply Nat.nlt_0_r in H; contradict H
+  end.
+                
+Ltac by_cell := 
+  intros;
+  let i := fresh "i" in 
+  let j := fresh "j" in 
+  let Hi := fresh "Hi" in 
+  let Hj := fresh "Hj" in 
+  intros i j Hi Hj; try solve_end;
+  repeat (destruct i as [|i]; simpl; [|apply lt_S_n in Hi]; try solve_end); clear Hi;
+  repeat (destruct j as [|j]; simpl; [|apply lt_S_n in Hj]; try solve_end); clear Hj.
+
+Ltac lma3 := by_cell; try lca.
+
+
+
+
+
+
+(*another approach but might be antithesis to the 'matrix is  function paradigm'
+  This can probably be made better with less helper functions that make the axiom
+  hard to prove  *)
+
+Fixpoint get_ps1 (n m : nat) : list (nat * nat) :=
+  match m with
+  | O    => [(n, m)]
+  | S m' => (n, m) :: get_ps1 n m'
+  end.
+
+Fixpoint get_ps (n m : nat) : list (nat * nat) :=
+  match n with
+  | O    => get_ps1 n m
+  | S n' => get_ps1 n m ++ get_ps n' m
+  end.
+
+Definition mtol {n m : nat} (M : Matrix n m) : list C :=
+  map (fun p =>  M (fst p) (snd p)) (get_ps (n - 1) (m - 1)). 
+
+
+Axiom mat_eq_list : forall {m n : nat} (A B : Matrix m n),
+  mtol A = mtol B <-> mat_equiv A B.
+
+
+
+
+
+
+
+(*                     *)
+
+
+
+
+
+
+
+
+
+
+(* Lemmas about Pauli and other common matrices *) 
+
+
+Lemma XtimesXid : Xgate × Xgate = I 2. 
+Proof. lma_super. (*now it almost seems like I am doing no work? It this the convention? Perhaps for computations...*)
+Qed.      
+
 Lemma YtimesYid : Ygate × Ygate = I 2.
-Proof. compute. prep_matrix_equality. do 3 (try destruct x; try destruct y; simpl; trivial). 
-       all : (try lca). rewrite -> Propff. lca.
+Proof. lma_super.
 Qed.
 
 Lemma ZtimesZid : Zgate × Zgate = I 2.
-Proof. compute. prep_matrix_equality. do 3 (try destruct x; try destruct y; simpl; trivial). 
-       all : (try lca). rewrite -> Propff. lca.
+Proof. lma_super.
 Qed.
 
 
 
 Lemma Y_eq_iXZ : Ygate = Ci .* Xgate × Zgate.
-Proof. compute. prep_matrix_equality. do 3 (try destruct x; try destruct y; simpl; trivial). 
-       all : lca. 
+Proof. lma_super.
 Qed.
 
 
 Lemma ZH_eq_HX : Zgate × Hgate = Hgate × Xgate.
 Proof. assert (H : Zgate × Hraw = Hraw × Xgate). 
-       { compute. prep_matrix_equality. 
-         do 3 (try destruct x; try destruct y; simpl; trivial).
-         all : lca. }
+       { lma_super. }
        unfold Hgate. rewrite Mscale_mult_dist_r. rewrite Mscale_mult_dist_l. rewrite H. 
        reflexivity. 
 Qed.
@@ -126,15 +303,12 @@ Qed.
 
 
 Lemma PX_eq_YP : Pgate × Xgate = Ygate × Pgate.
-Proof. compute. prep_matrix_equality. do 3 (try destruct x; try destruct y; simpl; trivial). 
-       all : lca. 
+Proof. lma_super.
 Qed.
 
 Lemma HtimesHid : Hgate × Hgate = I 2.
 Proof. assert (H : Hraw × Hraw = 2 .* I 2). 
-       { compute. prep_matrix_equality. 
-         do 3 (try destruct x; try destruct y; simpl; trivial).
-         all : (try lca). rewrite -> Propff. lca. }
+       { lma_super. }
        unfold Hgate. rewrite Mscale_mult_dist_r. rewrite Mscale_mult_dist_l.
        rewrite H. rewrite Mscale_assoc. rewrite Cinv_sqrt2_sqrt. rewrite Mscale_assoc.
        assert (H': / 2 * 2 = 1). { lca. } rewrite H'. rewrite Mscale_1_l. reflexivity.
@@ -142,9 +316,7 @@ Qed.
 
 Lemma XH_eq_HZ : Xgate × Hgate = Hgate × Zgate.
 Proof. assert (H : Xgate × Hraw = Hraw × Zgate). 
-       { compute. prep_matrix_equality. 
-         do 3 (try destruct x; try destruct y; simpl; trivial).
-         all : lca. }
+       { lma_super. }
        unfold Hgate. rewrite Mscale_mult_dist_r. rewrite Mscale_mult_dist_l. rewrite H. 
        reflexivity.
 Qed.
@@ -155,48 +327,34 @@ Definition is_unitary {n : nat} (A : Square n) : Prop :=
   A × (A†) = I n. 
 
 Lemma X_unitary : is_unitary Xgate.
-Proof. unfold is_unitary. compute. prep_matrix_equality. 
-       do 3 (try destruct x; try destruct y; simpl; trivial). 
-       all : (try lca). rewrite -> Propff. lca.
+Proof. lma_super. 
 Qed.
 
 Lemma Y_unitary : is_unitary Ygate.
-Proof. unfold is_unitary. compute. prep_matrix_equality. 
-       do 3 (try destruct x; try destruct y; simpl; trivial). 
-       all : (try lca). rewrite -> Propff. lca.
+Proof. lma_super. 
 Qed.
 
 Lemma Z_unitary : is_unitary Zgate.
-Proof. unfold is_unitary. compute. prep_matrix_equality. 
-       do 3 (try destruct x; try destruct y; simpl; trivial). 
-       all : (try lca). rewrite -> Propff. lca.
+Proof. lma_super.
 Qed.
 
 Lemma H_unitary : is_unitary Hgate.
 Proof. assert (H: Hgate† = Hgate). 
-       { unfold adjoint. compute. prep_matrix_equality.
-         do 3 (try destruct x; try destruct y; simpl; trivial). all : (try lca). }
+       { lma_super. }
        unfold is_unitary. rewrite H. apply HtimesHid.
 Qed.
 
 Lemma P_unitary : is_unitary Pgate.
-Proof. unfold is_unitary. compute. prep_matrix_equality. 
-       do 3 (try destruct x; try destruct y; simpl; trivial). 
-       all : (try lca). rewrite -> Propff. lca.
+Proof. lma_super.
 Qed.
 
 Lemma CNOT1_unitary : is_unitary CNOT1.
-Proof. unfold is_unitary. compute. prep_matrix_equality. 
-       do 5 (try destruct x; try destruct y; simpl; trivial). 
-       all : (try lca). rewrite -> Propff. lca.
+Proof. lma_super.
 Qed.
 
 Lemma CNOT2_unitary : is_unitary CNOT2.
-Proof. unfold is_unitary. compute. prep_matrix_equality. 
-       do 5 (try destruct x; try destruct y; simpl; trivial). 
-       all : (try lca). rewrite -> Propff. lca.
+Proof. lma_super.
 Qed.
-
 
 
 (* defining Heisenberg representation *)
@@ -207,8 +365,11 @@ Definition gate_type {n : nat} (U A B : Square n) : Prop :=
 Definition gate_app {n : nat} (U A : Square n) : Square n :=
   U × A × U†.
 
-Notation "U : A ~> B" := (gate_type U A B) (at level 0) : matrix_scope. 
-Notation "U ( A )" := (gate_app U A) (at level 0) : matrix_scope. 
+
+
+
+Notation "U : A → B" := (gate_type U A B) (at level 0) : matrix_scope. 
+Notation "U [ A ]" := (gate_app U A) (at level 0) : matrix_scope. 
 
 
 
@@ -218,8 +379,14 @@ Notation "U ( A )" := (gate_app U A) (at level 0) : matrix_scope.
 Axiom Mmult_1_r: forall (n : nat) (A : Square n),
   A × I n = A.
 
+Axiom Mmult_1_l: forall (n : nat) (A : Square n),
+  I n × A = A.
+
+
+
+  
 Lemma type_is_app : forall (n: nat) (U A B : Square n),
-  is_unitary U -> (U : A ~> B <-> U(A) = B).
+  is_unitary U -> (U : A → B <-> U[A] = B).
 Proof. intros n U A B H. split.
        - unfold gate_type; unfold gate_app. intros H'. unfold is_unitary in H. rewrite H'.
          rewrite Mmult_assoc. rewrite H. rewrite Mmult_1_r. reflexivity. 
@@ -241,69 +408,99 @@ Definition CNOT1_app := gate_app CNOT1.
 Definition CNOT2_app := gate_app CNOT2.
 
 
-Lemma HonX : Hgate : Xgate ~> Zgate.
+Lemma HonX : Hgate : Xgate → Zgate.
 Proof. unfold gate_type. rewrite ZH_eq_HX. easy.
 Qed.
 
-Lemma HonZ : Hgate : Zgate ~> Xgate.
+Lemma HonZ : Hgate : Zgate → Xgate.
 Proof. unfold gate_type. symmetry. apply XH_eq_HZ.
 Qed.
 
-Lemma PonX : Pgate : Xgate ~> Ygate.
+Lemma PonX : Pgate : Xgate → Ygate.
 Proof. unfold gate_type. apply PX_eq_YP.
 Qed.
 
 
 
-Lemma PonZ : Pgate : Zgate ~> Zgate.
-Proof. unfold gate_type. lma. (* did not prove above *)
-       compute. prep_matrix_equality. do 3 (try destruct x; try destruct y; simpl; trivial). 
-       all : lca.
+Lemma PonZ : Pgate : Zgate → Zgate.
+Proof. unfold gate_type. lma_super. 
 Qed.
 
-Lemma CNOT1onX1 : CNOT1 : (X 1) ~> (X 1 × X 2). 
-Proof. do 3 (try destruct x; try destruct y; simpl; trivial). 
 
-Lemma CNOT1onX2 : CNOT1 : (X 2) ~> (X 2). 
+Lemma X2iscorrect : X2 = X 2.
+Proof. lma_super. rewrite Propff. lca. 
+Qed.       
+
+
+Lemma CNOT1onX1 : CNOT1 : (X 1) → (X 1 × X 2). 
+Proof. Admitted. 
+
+Lemma CNOT1onX2 : CNOT1 : (X 2) → (X 2). 
+Proof. Admitted. 
+       
+Lemma CNOT1onZ1 : CNOT1 : (Z 1) → (Z 1). 
 Proof. Admitted.
 
-Lemma CNOT1onZ1 : CNOT1 : (Z 1) ~> (Z 1). 
-Proof. Admitted.
-
-Lemma CNOT1onZ2 : CNOT1 : (Z 2) ~> (Z 1 × Z 2). 
+Lemma CNOT1onZ2 : CNOT1 : (Z 2) → (Z 1 × Z 2). 
 Proof. Admitted.
 
 
-Lemma CNOT2onX1 : CNOT2 : (X 1) ~> (X 1). 
+Lemma CNOT2onX1 : CNOT2 : (X 1) → (X 1). 
 Proof. Admitted.
 
-Lemma CNOT2onX2 : CNOT2 : (X 2) ~> (X 1 × X 2). 
+Lemma CNOT2onX2 : CNOT2 : (X 2) → (X 1 × X 2). 
 Proof. Admitted.
 
-Lemma CNOT2onZ1 : CNOT2 : (Z 1) ~> (Z 1 × Z 2). 
+Lemma CNOT2onZ1 : CNOT2 : (Z 1) → (Z 1 × Z 2). 
 Proof. Admitted.
 
-Lemma CNOT2onZ2 : CNOT2 : (Z 2) ~> (Z 1). 
+Lemma CNOT2onZ2 : CNOT2 : (Z 2) → (Z 1). 
 Proof. Admitted.
 
 (* lemmas about heisenberg representation *)
 
-Lemma app_comp : forall (n : nat) (U A B C : Square n),
-  U(A) = B -> U(B) = C -> (U×U) (A) = C.
-Proof. unfold gate_app. intros n U A B C H1 H2. rewrite Mmult_adjoint. do 2 rewrite Mmult_assoc.
-       assert (H: U × (A × (U† × U†)) = U × A × U† × U† ). { do 2 rewrite Mmult_assoc. reflexivity. }
-       rewrite H. rewrite H1. rewrite <- Mmult_assoc. rewrite H2. reflexivity.
+Lemma app_comp : forall (n : nat) (U1 U2 A B C : Square n),
+  U1[A] = B -> U2[B] = C -> (U2×U1) [A] = C.
+Proof. unfold gate_app. intros n U1 U2  A B C H1 H2. rewrite <- H2. rewrite <- H1.
+       rewrite Mmult_adjoint. do 3 rewrite <- Mmult_assoc. reflexivity. 
 Qed.
 
 Lemma app_mult : forall (n : nat) (U A1 B1 A2 B2 : Square n),
-  is_unitary U -> U(A1) = B1 -> U(A2) = B2 -> U(A1 × A2) = B1×B2.
-Proof. intros n U A1 B1 A2 B2. unfold gate_app. intros H0 H1 H2. rewrite <- H1. rewrite <- H2.
-       assert (H: U† × (U × A2 × U†) = U† × U × A2 × U†). { do 2 rewrite <- Mmult_assoc. reflexivity. }
+  is_unitary U -> U[A1] = B1 -> U[A2] = B2 -> U[A1 × A2] = B1×B2.
+Proof. intros n U A1 B1 A2 B2. unfold gate_app. intros H0 H1 H2.
+       rewrite <- H1. rewrite <- H2.
+       assert (H: U† × (U × A2 × U†) = U† × U × A2 × U†).
+         { do 2 rewrite <- Mmult_assoc. reflexivity. }
        do 3 rewrite Mmult_assoc. rewrite H. unfold is_unitary in H0.
-       apply Minv_flip in H0. rewrite H0. do 4 rewrite <- Mmult_assoc. assert (H': U × A1 × I n = U × A1).
-       { rewrite Mmult_assoc. rewrite Mmult_1_r. reflexivity. } rewrite H'. reflexivity.
-       
+       apply Minv_flip in H0. rewrite H0. do 4 rewrite <- Mmult_assoc.
+       assert (H': U × A1 × I n = U × A1).
+         { rewrite Mmult_assoc. rewrite Mmult_1_r. reflexivity. }
+       rewrite H'. reflexivity.       
 Qed. 
+
+
+
+(* these lemmas seemed excessive. For some reason I could    *)
+(* not rewrite (Xgate ⊗ I 2) × (Xgate ⊗ I 2)without them..   *)
+Lemma X1timesX1id :  (Xgate ⊗ I 2) × (Xgate ⊗ I 2) = I 4.
+Proof. unfold X. rewrite kron_mixed_product. rewrite XtimesXid. rewrite Mmult_1_r.
+       rewrite id_kron. simpl. easy.
+Qed.
+
+Lemma X2timesX2id :  (I 2 ⊗ Xgate) × (I 2 ⊗ Xgate) = I 4.
+Proof. unfold X. rewrite kron_mixed_product. rewrite XtimesXid. rewrite Mmult_1_r.
+       rewrite id_kron. simpl. easy.
+Qed.
+
+
+Lemma XntimesXnid : forall (n : nat), X n × X n = I 4.
+Proof. destruct n. simpl. rewrite Mmult_1_r. reflexivity.
+       destruct n. rewrite <- X1timesX1id. unfold X. reflexivity.
+       destruct n. rewrite <- X2timesX2id. unfold X. reflexivity.
+       simpl. rewrite Mmult_1_r. reflexivity.
+Qed. 
+
+ 
 
 
 
@@ -311,17 +508,28 @@ Qed.
 
 Definition U1 : Matrix 4 4 := CNOT1 × CNOT2 × CNOT1.
 
-Lemma U1onX1 : U1 : (X 1) ~> (X 2).
-Proof. unfold gate_app. unfold U1. (rewrite <- Mmult_1_r) X 1. rewrite <- Mmult_1_r.
-
-
-
-unfold is_unitary. compute. prep_matrix_equality. 
-       do 3 (try destruct x; try destruct y; simpl; trivial). 
-       all : (try lca). rewrite -> Propff. lca.
+Lemma U1onX1 : U1 : (X 1) → (X 2).
+Proof. unfold U1. assert (H1: CNOT1[X 1] = (X 1 × X 2)).
+       { apply type_is_app. apply CNOT1_unitary. apply CNOT1onX1. }
+       assert (H2: CNOT2[X 1] = (X 1)).
+       { apply type_is_app. apply CNOT2_unitary. apply CNOT2onX1. }
+       assert (H3: CNOT2[X 2] = (X 1 × X 2)).
+       { apply type_is_app. apply CNOT2_unitary. apply CNOT2onX2. }
+       assert (H4: CNOT2[(X 1) × (X 2)] = (X 1) × (X 1 × X 2)).
+       { apply app_mult. apply CNOT2_unitary. apply H2. apply H3. }
+       assert (H5: X 1 × (X 1 × X 2) = X 2). 
+       { rewrite <- Mmult_assoc. rewrite XntimesXnid. rewrite Mmult_1_l. reflexivity. }   
+       rewrite H5 in H4. assert (H6: (CNOT2 × CNOT1)[X 1] = X 2).
+       { apply (app_comp 4 CNOT1 CNOT2 (X 1) (X 1 × X 2)). apply H1. apply H4. }
+       assert (H7: CNOT1[X 2] = (X 2)).
+       { apply type_is_app. apply CNOT1_unitary. apply CNOT1onX2. }
+       rewrite Mmult_assoc. apply type_is_app.
+       - unfold is_unitary. lma2.
+       - apply (app_comp 4 (CNOT2 × CNOT1) CNOT1 (X 1) (X 2) (X 2)).
+         apply H6. apply H7.
 Qed.
-
-Lemma 
+       
+    
 
 Definition Zgatetwo : Matrix 2 2 := 
   list2D_to_matrix  
