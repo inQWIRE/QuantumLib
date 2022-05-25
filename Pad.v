c2a0335a6050f1ab182badb379737028c72ddc8b
BHAKTISHAH <shahbhakti21@gmail.com>
2022-05-25 13:07:29 -0500

    pad_ctrl'_unitary

--- Pad.v
+++ Pad.v
@@ -1,5 +1,6 @@
 Require Export Complex.
 Require Export Quantum.
+Require Export Eigenvectors.
 
 (** This file provides padding functions to extend a matrix to a larger space.
    This is useful for describing the effect of 1- and 2-qubit gates on a larger
@@ -42,6 +43,12 @@ Definition pad_ctrl (dim m n: nat) (u: Square 2) :=
 Definition if_matrix {n} (b : bool) (u1 u2 : Square n) : Square n :=
   if b then u1 else u2.
 
+Lemma WF_if_matrix : forall (n : nat) (b : bool) (u1 u2 : Square n), 
+WF_Matrix u1 -> WF_Matrix u2 -> WF_Matrix (if_matrix b u1 u2).
+Proof.
+  intros. destruct b; assumption.
+Qed.
+
 Notation "A <| b |> B" := (if_matrix b A B) (at level 30).
 
 Lemma if_matrix_mul : forall b n (A B C D : Square n),
@@ -77,7 +84,7 @@ Lemma lt_max : forall (a b : nat),
 Proof.
   intros. lia.
   Qed.
-                            
+                        
 Lemma pad_ctrl_eq : forall dim m n u, pad_ctrl dim m n u = pad_ctrl' dim m n u.
 Proof.
   intros. unfold pad_ctrl, pad_ctrl'. bdestruct_all; try easy.
@@ -118,12 +125,14 @@ Lemma WF_pad_ctrl : forall dim m n u, WF_Matrix u -> WF_Matrix (pad_ctrl dim m n
   all : apply WF_pad; rewrite H'; apply WF_plus; auto with wf_db.
 Qed.
 
+Search Init.Nat.min (_ <= _)%nat.
+
 Lemma WF_pad_ctrl' : forall dim m n u, WF_Matrix u -> WF_Matrix (pad_ctrl' dim m n u).
 Proof.
   intros. unfold pad_ctrl'. bdestruct_all; try easy.
-  - simpl; assert (Init.Nat.min m n = m). { lia. } rewrite H2;
+  - simpl; replace (Init.Nat.min m n) with m by lia. 
   apply WF_pad; restore_dims; auto with wf_db.
-  - simpl; assert (Init.Nat.min m n = n). { lia. } rewrite H2;
+  - simpl; replace (Init.Nat.min m n) with n by lia. 
   apply WF_pad; restore_dims; auto with wf_db.
   Qed.
 
@@ -133,7 +142,7 @@ Lemma WF_pad_swap : forall dim m n, WF_Matrix (pad_swap dim m n).
   repeat apply WF_mult; apply WF_pad_ctrl; apply WF_σx.
 Qed.
 
-#[export] Hint Resolve WF_pad WF_pad_u WF_pad_ctrl WF_pad_swap : wf_db.
+#[export] Hint Resolve WF_pad WF_pad_u WF_pad_ctrl WF_pad_ctrl' WF_if_matrix WF_pad_swap : wf_db.
 
 (** Unitarity *)
 
@@ -156,6 +165,47 @@ Lemma pad_u_unitary : forall dim n u,
     WF_Unitary u ->
     WF_Unitary (pad_u dim n u).
 Proof. intros. apply pad_unitary. lia. auto. Qed.  
+
+Lemma pad_ctrl'_unitary : forall dim m n u,
+m <> n -> 
+(m < dim)%nat ->
+(n < dim)%nat ->
+WF_Unitary u ->
+WF_Unitary (pad_ctrl' dim m n u).
+Proof.
+  intros dim m n u neq_mn lm ln wfu.
+  destruct wfu.
+  unfold pad_ctrl', pad, WF_Unitary. bdestruct_all; simpl; try auto with wf_db.
+  + replace (Init.Nat.min m n) with m in * by lia; split; restore_dims.
+    - apply WF_kron; auto with wf_db.
+    - Msimpl; unfold abs_diff;
+    replace (Init.Nat.max (m - n) (n - m)) with ((n - m)%nat) by lia;
+    gridify; rewrite H0; Qsimpl;
+    replace (m + 1 + d + 1 + x - (m + S (d + 1)))%nat with x by lia;
+    repeat rewrite kron_assoc; try auto with wf_db;
+    repeat rewrite id_kron; restore_dims;
+    rewrite <- kron_plus_distr_l; rewrite <- kron_plus_distr_r;
+    rewrite Mplus10; repeat rewrite id_kron; reflexivity.
+  + split; restore_dims.
+    - auto with wf_db.
+    - unfold abs_diff in H2; gridify.
+  + replace (Init.Nat.min m n) with n by lia; unfold abs_diff;
+  replace (Init.Nat.max (m - n)(n - m)) with (m - n)%nat by lia.
+  split; restore_dims.
+    - apply WF_kron; auto with wf_db.
+    - Msimpl; gridify; Qsimpl; rewrite H0; 
+    replace (n + 1 + x1 - n - 1)%nat with x1 by lia;
+    repeat rewrite <- kron_assoc; try auto with wf_db;
+    repeat rewrite id_kron; restore_dims;
+    rewrite <- kron_plus_distr_r; rewrite <- kron_plus_distr_l;
+    rewrite Mplus10; repeat rewrite id_kron;
+    replace (n + 1 + (x1 + S x0) - (n + S (x1 + 1)))%nat with x0 by lia; reflexivity.
+  + split; restore_dims.
+  - auto with wf_db.
+  - unfold abs_diff in H2; gridify.
+  Qed.
+
+
 
 Lemma pad_ctrl_unitary : forall dim m n u,
     m <> n ->
@@ -226,6 +276,49 @@ Proof.
   unfold pad_ctrl, pad_u, pad.
   gridify; trivial.
 Qed.
+
+(* theorems and lemmas about diagonalizability of matrices might be helpful
+and make the next two proofs a lot faster.  *)
+
+Lemma pad_A_ctrl'_commutes :  forall dim m n o A B,
+m <> n ->
+m <> o ->
+WF_Matrix A ->
+WF_Matrix B ->
+pad_u dim m A × pad_ctrl' dim n o B = pad_ctrl' dim n o B × pad_u dim m A.
+Proof.
+  intros. unfold pad_ctrl', pad_u, pad, abs_diff.
+  bdestruct_all; try auto with wf_db.
+  - replace (Init.Nat.min n o) with n in * by lia.
+  replace (Init.Nat.max (n - o) (o - n)) with (o - n)%nat in * by lia.
+  replace (n + (1 + (o - n - 1) + 1))%nat with (o + 1)%nat in * by lia.
+  simpl in *. 
+  assert (WF_Diagonal (I (2 ^ m) ⊗ A ⊗ I (2 ^ (dim - (m + 1))))).
+  + unfold WF_Diagonal. split. auto with wf_db.
+  intros. gridify.
+  Admitted.
+
+
+Lemma pad_ctrl'_ctrl'_commutes : forall dim m n o p A B,
+  m <> o ->
+  m <> p ->
+  n <> o ->
+  n <> p ->
+  WF_Matrix A ->
+  WF_Matrix B ->
+  pad_ctrl' dim m n A × pad_ctrl' dim o p B = pad_ctrl' dim o p B × pad_ctrl' dim m n A.
+Proof.
+  intros. unfold pad_ctrl', pad, abs_diff. bdestruct_all; simpl; 
+  try auto with wf_db.
+  + replace (Init.Nat.min m n) with m in * by lia.
+    replace (Init.Nat.min o p) with o in * by lia.
+    replace (Init.Nat.max (m - n) (n - m)) with (n - m)%nat in * by lia.
+    replace (Init.Nat.max (o - p) (p - o)) with (p - o)%nat in * by lia.
+    
+
+
+
+
 
 (* Horribly slow due to many subcases.
    TODO: can we speed this up be tweaking the ordering in gridify? *)
